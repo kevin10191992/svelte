259950857ba9afb3084d0a87fd4b733e8d2293b1
kperez <kevin10191992@hotmail.com>
2021-11-01 16:54:45 -0500

    add metadata

--- sveltesapper/src/routes/index.svelte
+++ sveltesapper/src/routes/index.svelte
@@ -3,6 +3,23 @@
 </script>
 
 <svelte:head>
+  <meta name="description" content="Blog de live" />
+
+  <meta name="twitter:card" content="summary_large_image" />
+  <meta name="twitter:site" content="@test" />
+  <meta name="twitter:creator" content="@test" />
+  <meta name="twitter:title" content="ejemplos" />
+  <meta name="twitter:description" content="Blog de " />
+  <meta name="twitter:image" content="https://arepa.s3.amazonaws.com/blogpost-cover.png" />
+
+  <meta name="og:title" content="test" />
+  <meta name="og:description" content="Blog de live" />
+  <meta name="og:image" content="https://arepa.s3.amazonaws.com/blogpost-cover.png" />
+  <meta name="og:url" content="" />
+  <meta name="og:site_name" content="test ejemplo" />
+  <meta name="og:locale" content="es_CO" />
+  <meta name="og:type" content="article" />
+  <meta name="fb:app_id" content="" />
   <title>Sapper project template</title>
 </svelte:head>
 
